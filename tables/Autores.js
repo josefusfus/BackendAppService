9d738300d5c63f5b668e816ddb21a1d61c1656fb
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-10-30 20:53:02 +0100

    Crear tabla de autor sin cambiar esquema ni asignar autenticacion

---
+++ tables/Autores.js
@@ -0,0 +1,20 @@
+/**
+ * Created by joseluissaezsanchez on 30/10/16.
+ */
+
+var azureMobileApps = require('azure-mobile-apps');
+
+var table = azureMobileApps.table(); //Crea la tabla con el mismo nombre dle fichero que estamos
+
+table.columns = {
+
+    "nombre" : "String",
+    "secondName" : "String"
+
+};
+
+module.exports = table;
+
+
+
+
