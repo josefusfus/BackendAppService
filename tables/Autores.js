53549c84332f94da39d81e1f7ac3ba085cad66e9
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-10-31 00:37:35 +0100

    Poniendo autenticado la lectura y la actualización en el servicio

--- tables/Autores.js
+++ tables/Autores.js
@@ -18,8 +18,8 @@ table.columns = {
  */
 
 
-table.read.access = 'auhenticated';
-table.update.access = 'disabled';
+table.read.access = 'authenticated';
+table.update.access = 'authenticated';
 
 module.exports = table;
 
