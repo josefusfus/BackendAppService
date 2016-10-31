86e5af3c0dc1ccf45d03512d0784e2fe84fbfabf
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-10-31 01:41:13 +0100

    Creamos una api custom

---
+++ api/readAllRecords.js
@@ -0,0 +1,32 @@
+/**
+ * Created by joseluissaezsanchez on 31/10/16.
+ */
+
+
+var api = {
+
+    get : function (req,res,next) {
+
+        if (typeof req.params.length < 0) {
+
+            return next();
+        }
+
+        var context = req.azureMobile;
+        var user = context.user.id;
+
+        var query = {
+
+            sql: "Select * FROM Autores"
+        };
+
+        req.azureMobile.data.execute(query)
+            .then(function (result) {
+            res.json(result);
+        });
+
+    }
+};
+
+api.get.access = 'authenticated';
+module.exports = api;
--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var express = require("express"),
     azuremobileapps = require("azure-mobile-apps");
 
 var app = express(),
-    mobile = azuremobileapps();
-   //  mobile = azuremobileapps( { swagger: process.env.NODE_ENV !== 'production'}); asi lo dejamos cuando esta en desarrollo, swager activado es peligroso
+    mobile = azuremobileapps( { swagger: process.env.NODE_ENV !== 'production'}); 
 
 
 
