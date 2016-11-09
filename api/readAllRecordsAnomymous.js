60ee9d65e669174320365ea67f117fb37f444a22
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-11-09 01:10:21 +0100

    Probando con sid especificado

---
+++ api/readAllRecordsAnomymous.js
@@ -0,0 +1,32 @@
+/**
+ * Created by joseluissaezsanchez on 9/11/16.
+ */
+
+
+var api = {
+
+    get : function (req,res,next) {
+
+        if (typeof req.params.length < 0) {
+
+            return next();
+        }
+
+        var context = req.azureMobile;
+
+
+        var query = {
+
+            sql: "SELECT * FROM Noticias WHERE published = 1"
+        };
+
+        req.azureMobile.data.execute(query)
+            .then(function (result) {
+                res.json(result);
+            });
+
+    }
+};
+
+api.get.access = 'anonymous';
+module.exports = api;
